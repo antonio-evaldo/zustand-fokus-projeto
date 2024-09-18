d0b6954b6e344f5f30b28868716d190c6a42a727
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-09-18 16:08:05 -0300

    remove erro de digitação

--- src/components/Cronometro/index.jsx
+++ src/components/Cronometro/index.jsx
@@ -24,7 +24,6 @@ export default function Cronometro({ modoCronometro, setModoCronometro }) {
             >
               {botao.texto}
             </BotaoModo>
-            ;
           </li>
         ))}
       </ul>
